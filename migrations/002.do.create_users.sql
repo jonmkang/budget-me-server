d96d7b652b9683dd744c9868488ae3638b0ac21c
Jonathan Kang <jon.m.kang@gmail.com>
2020-09-28 11:46:51 -0400

    fixed syntax

--- migrations/002.do.create_users.sql
+++ migrations/002.do.create_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE budget_user (
     user_id SERIAL PRIMARY KEY,
     user_email TEXT NOT NULL,
     user_password TEXT NOT NULL
-)
+);
 
 ALTER TABLE category
     ADD COLUMN user_id INTEGER REFERENCES budget_user(user_id) ON DELETE SET NULL;
